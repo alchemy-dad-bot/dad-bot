0be751c9d16f0521eb31ed7a07241d513200e395
Austin Han <austinbhan@outlook.com>
2022-08-26 10:08:35 -0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const app = require('./lib/app');
+const pool = require('./lib/utils/pool');
+
+const API_URL = process.env.API_URL || 'http://localhost';
+const PORT = process.env.PORT || 7890;
+
+app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log(`🚀  Server started on ${API_URL}:${PORT}`);
+});
+
+process.on('exit', () => {
+  // eslint-disable-next-line no-console
+  console.log('👋  Goodbye!');
+  pool.end();
+});
---
+++ setup-db.js
@@ -0,0 +1,7 @@
+/* eslint-disable no-console */
+const pool = require('./lib/utils/pool');
+const setup = require('./data/setup');
+
+setup(pool)
+  .catch((err) => console.error(err))
+  .finally(() => process.exit());
