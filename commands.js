0ee06e762a46958aa9b45aae3548d7d502abfc9f
Brien Thomas <thomasbrien94@gmail.com>
2022-08-29 13:02:22 -0700

    working through the first slash command builder, failing to set token still

---
+++ commands.js
@@ -0,0 +1,19 @@
+const { SlashCommandBuilder, Routes } = require('discord.js');
+const { REST } = require('@discordjs/rest');
+
+
+const commands = [
+  new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
+  new SlashCommandBuilder().setName('server').setDescription('Replies with server info!'),
+  new SlashCommandBuilder().setName('user').setDescription('Replies with user info!'),
+]
+  .map(command => command.toJSON());
+
+const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
+
+rest.put(Routes.applicationGuildCommands(
+  process.env.APP_ID, 
+  process.env.GUILD_ID), 
+{ body: commands })
+  .then((data) => console.log(`Successfully registered ${data.length} application commands.`))
+  .catch(console.error);
