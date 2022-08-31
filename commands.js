d2950adb4f40356b0886f5c24fce4c67d9c64b94
austinbhan <austinbhan@outlook.com>
2022-08-31 12:46:37 -0700

    Delete in progress

--- commands.js
+++ commands.js
@@ -17,7 +17,7 @@ const commands = [
         .setRequired(true)),
   new SlashCommandBuilder().setName('my-jokes').setDescription('A list of your jokes'),    
   new SlashCommandBuilder().setName('get-dads').setDescription('A list of dads'),
-  
+  new SlashCommandBuilder().setName('delete-joke').setDescription('Deletes a user joke') // Currently Working on
 ]
   .map(command => command.toJSON());
 
--- index.js
+++ index.js
@@ -56,6 +56,7 @@ client.on('interactionCreate', async (interaction) => {
   } else if (commandName === 'my-jokes') {
     const joke = await User.getRandomJoke({ user_id: interaction.user.id });
     interaction.reply(joke.content);
+    console.log('random user created joke', joke);
     console.log(interaction.reply);
   } else if (commandName === 'get-dads') {
     const dad = await Creator.getCreators();
@@ -68,6 +69,12 @@ client.on('interactionCreate', async (interaction) => {
       ${ dad[3].name }, ${ dad[3].linkedin}, ${dad[3].github}
       `
     });
+  }
+  else if (commandName === 'delete-joke') {
+    const deleteJoke = await User.deleteUserJoke({ 
+      id: interaction.id });
+    console.log('line 75', deleteJoke);
+    interaction.reply(deleteJoke);
   }
 });
 
