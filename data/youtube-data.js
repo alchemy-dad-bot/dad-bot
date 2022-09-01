c55e4eaddff9f3124829a122ce020b7f9395ec58
Brien Thomas <thomasbrien94@gmail.com>
2022-08-31 17:54:36 -0700

    We were able to return a list of mapped videos to the discord bot, still working on functionality to continue the interaction, and get that video to display in the channel

--- data/youtube-data.js
+++ data/youtube-data.js
@@ -1,13 +1,14 @@
-
+const fetch = require('cross-fetch');
 
 async function getDadHelp(query) {
   try {
-    const res = await fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=${process.env.YOUTUBE_CHANNEL_ID}&q=${query}`, {
+    const res = await fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=${process.env.YOUTUBE_CHANNEL_ID}&q=${query}&key=${process.env.YOUTUBE_API_KEY}`, {
       headers: { 
-        Accept: 'application/json',
-        key: process.env.YOUTUBE_API_KEY
+        key: process.env.YOUTUBE_API_KEY,
+        Accept: 'application/json'
       }
     });
+    console.log('res from youtube.dada', res);
     const search = await res.json();
       
     if (res.status >= 400) {
